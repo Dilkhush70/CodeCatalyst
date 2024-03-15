b124c2d8ab35ded2baf6ebce5213952916ab62ad
Dilkhush70 <dilkhushkumar1610@gmail.com>
2024-03-15 15:21:05 +0530

    last

---
+++ Js/cart.js
@@ -0,0 +1,32 @@
+var p1,p2,p3,p4,p5,p6,p7,p8;
+function product1(){
+p1++;
+document.cookie = "prd1=p1";
+}
+
+function product2(){
+    p2++;
+    document.cookie = "prd2=p2";
+    }
+
+    function product3(){
+        p3++;
+        document.cookie = "prd3=p3";
+        }
+
+
+        function product4(){
+            p4++;
+            document.cookie = "prd4=p4";
+            }
+
+            function product5(){
+                p5++;
+                document.cookie = "prd5=p5";
+                }
+
+
+                function product6(){
+                    p6++;
+                    document.cookie = "prd6=p6";
+                    }
